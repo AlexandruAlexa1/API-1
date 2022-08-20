056bc25e84a83e5a1def7eebccb5552249635fcb
Alexandru Alexa <x.alx@yahoo.com>
2022-08-20 02:32:13 -0800

    Update Entity

--- src/main/java/com/aa/entity/Customer.java
+++ src/main/java/com/aa/entity/Customer.java
@@ -1,32 +1,59 @@
 package com.aa.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "customers")
 public class Customer {
-
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int id;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer id;
+	
+	@Column(name = "first_name", nullable = false, length = 45)
 	private String firstName;
 	
+	@Column(name = "last_name", nullable = false, length = 45)
 	private String lastName;
 	
+	@Column(length = 45, nullable = false, unique = true)
 	private String email;
 	
+	@Column(length = 65, nullable = false)
+	private String password;
+	
+	@Column(name = "address_line_1", nullable = false, length = 64)
+	private String addressLine1;
+	
+	@Column(name = "address_line_2", length = 64)
+	private String addressLine2;
+	
+	@Column(nullable = false, length = 45)
+	private String city;
+	
+	@Column(nullable = false, length = 45)
+	private String state;
+	
+	@Column(name = "postal_code")
+	private String postalCode;
+	
+	@Column(name = "phone_number", nullable = false, length = 15)
+	private String phoneNumber;
+
 	public Customer() {
 		
 	}
 
-	public int getId() {
+	public Integer getId() {
 		return id;
 	}
 
-	public void setId(int id) {
+	public void setId(Integer id) {
 		this.id = id;
 	}
 
@@ -52,5 +79,67 @@ public class Customer {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddressLine1() {
+		return addressLine1;
+	}
+
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	public String getAddressLine2() {
+		return addressLine2;
+	}
+
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", addressLine1=" + addressLine1 + "]";
 	}
 }
